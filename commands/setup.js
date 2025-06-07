7e2c9e3789609382f917e7f6d0244952cfaa6942
zonekrish <krishna.rao0626@gmail.com>
2025-06-06 19:39:50 -0500

    Finish setup?

--- commands/setup.js
+++ commands/setup.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, EmbedBuilder, ChannelType} = require("discord.js");
+const {SlashCommandBuilder, EmbedBuilder, ChannelType, ButtonBuilder, ButtonStyle, ActionRowBuilder} = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,14 +11,24 @@ module.exports = {
         let embed = new EmbedBuilder()
             .setColor(0x700434)
             .setTitle("Setup")
-            .setDescription("A text channel named \`cord-connect\` has already been created!");
+            .setDescription("A text channel named \`cord-connect\` has already been created!\n\nWould you like to reconnect this bot to a different Minecraft server?");
+        
+        let button = new ButtonBuilder()
+            .setCustomId("connect")
+            .setLabel("Reconnect")
+            .setStyle(ButtonStyle.Danger);
 
         if (!channel) {
             embed = new EmbedBuilder()
                 .setColor(0x700434)
                 .setTitle("Setup")
-                .setDescription("A text channel named \`cord-connect\` has been created! Here, updates from your Minecraft server's chat will be posted.");
-
+                .setDescription("A text channel named \`cord-connect\` has been created! Here, updates from your Minecraft server's chat will be posted.\n\nPress the button below to connect this bot to a Minecraft server.");
+            
+            button = new ButtonBuilder()
+                .setCustomId("connect")
+                .setLabel("Connect")
+                .setStyle(ButtonStyle.Danger);
+            
             interaction.guild.channels.create({
                 name: "cord-connect",
                 type: ChannelType.GuildText
@@ -27,7 +37,10 @@ module.exports = {
            
         }
 
-        await interaction.reply({embeds:[embed]});
+        const row = new ActionRowBuilder()
+            .addComponents(button);
+
+        await interaction.reply({embeds:[embed], components:[row]});
 
         
     }
