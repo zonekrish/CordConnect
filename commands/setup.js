3c32e47caec52c1b4837180ab466dc402ff96d9a
zonekrish <krishna.rao0626@gmail.com>
2025-06-06 19:24:06 -0500

    Finish setup phase 1

--- commands/setup.js
+++ commands/setup.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, EmbedBuilder} = require("discord.js");
+const {SlashCommandBuilder, EmbedBuilder, ChannelType} = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,10 +6,29 @@ module.exports = {
     .setDescription("Sets up CordConnect for your server"),
 
     async execute(interaction) {
-        const embed = new EmbedBuilder()
+        let channel = interaction.guild.channels.cache.find(channel => channel.name === "cord-connect");
+
+        let embed = new EmbedBuilder()
             .setColor(0x700434)
             .setTitle("Setup")
+            .setDescription("A text channel named \`cord-connect\` has already been created!");
+
+        if (!channel) {
+            embed = new EmbedBuilder()
+                .setColor(0x700434)
+                .setTitle("Setup")
+                .setDescription("A text channel named \`cord-connect\` has been created! Here, updates from your Minecraft server's chat will be posted.");
+
+            interaction.guild.channels.create({
+                name: "cord-connect",
+                type: ChannelType.GuildText
+            });
+
+           
+        }
 
         await interaction.reply({embeds:[embed]});
+
+        
     }
 }
